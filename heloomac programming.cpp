5e88106721d9fe491c48de0b1da3d9cd397804a6
Jeshika Chaulagain <jeshika.21chaulagain@gmail.com>
2025-09-25 19:43:10 -0400

    Add files via upload

---
+++ heloomac programming.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std;
+
+int main() 
+{
+  string name;
+   std:: cout << "Enter your name:";
+   cin >> name; 
+   string Fav;
+   std:: cout << "what is ur Fav color:" << Fav;
+   cin >> Fav;
+   string animal;
+   std:: cout <<  "what is ur animal:" << animal;
+   cin>> animal;
+    cout << "Your pretty name is " << name << ", your fav color is " << Fav 
+         << ", and your favorite animal is " << animal << "." << endl;
+
+   return 0;
+
+
+
+}
+    
